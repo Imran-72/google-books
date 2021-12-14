bad429fab700f8207ebffd2186184e4f0caf70a0
Imran-72 <petrow318@gmail.com>
2021-12-14 17:43:34 +0300

    установил redux-thunk и получил промис

---
+++ src/redux/appReducer.js
@@ -0,0 +1,16 @@
+import { SHOW_LOADER, HIDE_LOADER } from "./types";
+
+const initialState = {
+  loading: false,
+};
+
+export const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SHOW_LOADER:
+      return { ...state, loading: true };
+    case HIDE_LOADER:
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+};
