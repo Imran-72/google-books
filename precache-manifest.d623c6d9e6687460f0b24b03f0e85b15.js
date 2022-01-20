cde0acc1959f05ea7f5987bb23f9f411cbc2c0bd
Imran-72 <petrow318@gmail.com>
2022-01-20 13:05:42 +0300

    Updates

---
+++ precache-manifest.d623c6d9e6687460f0b24b03f0e85b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7910fa5e836330c91449a3e7f6d432d",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "91d390441773ee18c318",
+    "url": "/google-books/static/css/2.a8ba0f7c.chunk.css"
+  },
+  {
+    "revision": "f200262533203c660bf9",
+    "url": "/google-books/static/css/main.a46fbaa7.chunk.css"
+  },
+  {
+    "revision": "91d390441773ee18c318",
+    "url": "/google-books/static/js/2.1bc6893c.chunk.js"
+  },
+  {
+    "revision": "fdf611e81b8e87d9d749523e1daffe94",
+    "url": "/google-books/static/js/2.1bc6893c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f200262533203c660bf9",
+    "url": "/google-books/static/js/main.3cf646c6.chunk.js"
+  },
+  {
+    "revision": "ee2d4cd0db6fc595c9bb",
+    "url": "/google-books/static/js/runtime-main.e0ad9b41.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2cb24afc23cc5d471a1f70fcbae8ca5.js"
+  "/google-books/precache-manifest.d623c6d9e6687460f0b24b03f0e85b15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/google-books/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
