1ec9ae6766baabecfa27b3193df9b106eb8e4d39
Imran-72 <petrow318@gmail.com>
2022-01-20 13:00:36 +0300

    Updates

---
+++ precache-manifest.f2cb24afc23cc5d471a1f70fcbae8ca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65e5fa45286f03d8afb2c094143c523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7b172aa503ffd6ea21",
+    "url": "/static/css/2.a8ba0f7c.chunk.css"
+  },
+  {
+    "revision": "399ca630fa8f001b65f9",
+    "url": "/static/css/main.a46fbaa7.chunk.css"
+  },
+  {
+    "revision": "7c7b172aa503ffd6ea21",
+    "url": "/static/js/2.9cf8bd16.chunk.js"
+  },
+  {
+    "revision": "fdf611e81b8e87d9d749523e1daffe94",
+    "url": "/static/js/2.9cf8bd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399ca630fa8f001b65f9",
+    "url": "/static/js/main.f364b4fc.chunk.js"
+  },
+  {
+    "revision": "f85c5257fe4c0f03c90a",
+    "url": "/static/js/runtime-main.95daa00d.js"
+  }
+]);
